ae4032cdee951e2a81cf8522a85eeaba56b4b32a
Ann Diehl <ann@tttdevs.com>
2017-05-01 01:20:27 -0300

    license + gitignore

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php 
 
 /*
-Title: Simple Blog (codename: Byron)
-Author: Ann D.
-Description: This is a simple markdown blog-  
+  Title: Simple Blog (codename: Byron)
+  Author: Ann D. <ann@tttdevs.com>
+  Description: This is a simple markdown blog-  
+
+  Byron, markdown blogging platform.
+  Copyright (C) 2017  Ann Diehl
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // include  the library markdown parsing
